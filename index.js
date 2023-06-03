da45e1497fbbe31ca72e5add0eec178674e8c132
Ojjasvi jain <85935892+ojjasvi-jain@users.noreply.github.com>
2023-06-03 23:34:45 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,76 @@
+const bodyEl = document.querySelector("body");
+
+bodyEl.addEventListener("mousemove", (event) => {
+  const xPos = event.offsetX;
+  const yPos = event.offsetY;
+  const spanEl = document.createElement("span");
+  spanEl.style.left = xPos + "px";
+  spanEl.style.top = yPos + "px";
+  const size = Math.random() * 100;
+  spanEl.style.width = size + "px";
+  spanEl.style.height = size + "px";
+  bodyEl.appendChild(spanEl);
+  setTimeout(() => {
+    spanEl.remove();
+  }, 3000);
+});
+
+// ------------------------------------
+
+const imageContainerEl = document.querySelector(".image-container");
+
+const prevEl = document.getElementById("prev");
+const nextEl = document.getElementById("next");
+let x = 0;
+let timer;
+prevEl.addEventListener("click", () => {
+  x = x + 45;
+  clearTimeout(timer);
+  updateGallery();
+});
+nextEl.addEventListener("click", () => {
+  x = x - 45;
+  clearTimeout(timer);
+  updateGallery();
+});
+
+function updateGallery() {
+  imageContainerEl.style.transform = `perspective(1000px) rotateY(${x}deg)`;
+  timer = setTimeout(() => {
+    x = x - 45;
+    updateGallery();
+  }, 3000);
+}
+
+updateGallery();
+
+// ______________________________________________________________________
+
+const containerEl = document.querySelector(".container");
+
+const careers = ["brother 👱‍♂️", "Son👨", "teacher 👨‍💻", "sara-Ng 😁"];
+
+let careerIndex = 0;
+
+let characterIndex = 0;
+
+updateText();
+
+function updateText() {
+  characterIndex++;
+  containerEl.innerHTML = `
+    <h1>happy birthday to best ${
+      careers[careerIndex].slice(0, 1) === "I" ? "an" : "a"
+    } ${careers[careerIndex].slice(0, characterIndex)}</h1>
+    `;
+
+  if (characterIndex === careers[careerIndex].length) {
+    careerIndex++;
+    characterIndex = 0;
+  }
+
+  if (careerIndex === careers.length) {
+    careerIndex = 0;
+  }
+  setTimeout(updateText, 400);
+}
